839f34669988c7dd8a4c7dee52d6494c5a4ebe11
snakano <snakano@student.42tokyo.jp>
2020-04-14 17:08:30 +0900

    2020/04/14

--- src/current_time.cpp
+++ src/current_time.cpp
@@ -1,10 +1,12 @@
 #include <chrono>
 #include <iostream>
+#include <thread>
 
 int main()
 {
     auto time = std::chrono::system_clock::now();
     std::string time_s;
+    std::string time_s_old;
     int length;
 
     while (1) {
@@ -13,7 +15,10 @@ int main()
         time_s = std::ctime(&current_time);
         length = time_s.length();
         time_s = time_s.substr(0, length - 1);
-        std::cout << "\r" << time_s
-                  << std::flush;
+        if (time_s != time_s_old) {
+            std::cout << "\r" << time_s << std::flush;
+            time_s_old = time_s;
+            std::this_thread::sleep_for(std::chrono::microseconds(999));
+        }
     }
 }
